5ab9faeb8aeba323eae38788236f65ee472663da
K J <bindhu.k-j@capgemini.com>
2023-02-02 17:43:02 +0530

    Initial commit

---
+++ src/ASCIIValues/Example3.java
@@ -0,0 +1,11 @@
+package ASCIIValues;
+
+public class Example3 {
+    public static void main(String[] args)
+    {
+        for(int i = 0; i <= 255; i++)
+        {
+            System.out.println(" The ASCII value of " + (char)i + "  =  " + i);
+        }
+    }
+}
---
+++ src/ASCIIValues/Example4.java
@@ -0,0 +1,10 @@
+package ASCIIValues;
+
+public class Example4 {
+    public static void main(String[] args) {
+        for (int i = 65; i <= 90; i++) {
+            System.out.println(" The ASCII value of " + (char) i + "  =  " + i);
+        }
+    }
+}
+
